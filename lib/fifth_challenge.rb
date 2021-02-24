6e05cc383e4b62f67150389060a9214f25563825
julianxlozano <julianxlozano@gmail.com>
2021-02-24 17:59:06 -0600

    commit tree d683b1d3bf7efde80da9a0c2cac8509277299aa3

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -1,12 +1,14 @@
 require "pry"
 
 def fifth_challenge
+   
+#I had to hard code it because everytime I did      epic_tragedy[:montague][:hero][:status]="dead"      ....it gave me a no method error. 
 
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", status: "dead"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
@@ -15,7 +17,7 @@ def fifth_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
@@ -23,9 +25,17 @@ def fifth_challenge
    }
   }
 
-  #code your solution here:
+  hero1=epic_tragedy[:montague][:hero]
+
+ hero= epic_tragedy[:capulet][:hero]
+
+  
 
 
-  #Don't touch the following line! The method must return our newly modified epic tragedy hash
+
+
+
   epic_tragedy
 end
+
+
